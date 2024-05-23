a071cd99c3a0318462248762d8a650c24109b0f4
Antoine Gadrat <antoinegadrat@hotmail.fr>
2024-05-23 16:59:14 +0200

    feat:add de dynamic feature to upload new pics on the web site

--- FrontEnd/assets/scripts/components/category-component.js
+++ FrontEnd/assets/scripts/components/category-component.js
@@ -6,4 +6,42 @@ export const createCategoriesContainer= async () => {
     console.log(categoriesContainer, worksSection);
     const categories = await findAllCategories();
     console.log(categories);
-}
+    //ajout du li//
+    categories.forEach(category => {
+        const listItem = document.createElement("li");
+        listItem.textContent = category.name;
+        categoriesContainer.appendChild(listItem);
+    });
+    
+    worksSection.appendChild(categoriesContainer);
+};
+import { findAllWorks } from "../services/category-service.js";
+
+export async function createWorksContainer(){
+    try {
+        const works = await findAllWorks();
+        console.log(works);
+        const worksSection = document.getElementById("portfolio");
+        const gallery = worksSection.querySelector(".gallery");
+
+        gallery.innerHTML = '';
+
+        works.forEach(({title, imageUrl}) => {
+        const figureElement = document.createElement("figure");
+        const imageElement = document.createElement("img");
+        imageElement.src = imageUrl;
+        imageElement.alt = title;
+        const titleElement = document.createElement("h3");
+        titleElement.textContent = title;
+        const figure = document.querySelector(".gallery");
+
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(titleElement);
+        gallery.appendChild(figureElement);
+        })
+    }
+    catch (error) {
+        console.error("Error fetching or displaying works:", error);
+    
+    };
+};
--- FrontEnd/assets/scripts/scripts.js
+++ FrontEnd/assets/scripts/scripts.js
@@ -1,3 +1,6 @@
 import { createCategoriesContainer } from "./components/category-component.js";
-
+import { createWorksContainer } from "./components/category-component.js";
 createCategoriesContainer();
+createWorksContainer();
+document.addEventListener("DOMContentLoaded", createWorksContainer);
+
