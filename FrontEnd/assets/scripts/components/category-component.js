25e3d975c4099fd086899eb293aca64e76921c22
Antoine Gadrat <antoinegadrat@hotmail.fr>
2024-05-24 19:47:10 +0200

    fix: change position between functions workscontainer and categoriescontainer

--- FrontEnd/assets/scripts/components/category-component.js
+++ FrontEnd/assets/scripts/components/category-component.js
@@ -1,28 +1,5 @@
-import { findAllCategories } from "../services/category-service.js";
+// fonction pour ajouter tous les travaux//
 
-export const createCategoriesContainer= async () => {
-    const categoriesContainer = document.createElement("ul");
-    categoriesContainer.classList.add("listeFilter");
-    const worksSection = document.getElementById("portfolio");
-    
-    console.log(categoriesContainer, worksSection);
-    const categories = await findAllCategories();
-    console.log(categories);
-    worksSection.appendChild(categoriesContainer);
-    //ajout du li//
-    categories.forEach(category => {
-        const btnFilter = document.createElement("li");
-        btnFilter.textContent = category.name;
-        btnFilter.classList.add("btnFilter");
-        categoriesContainer.appendChild(btnFilter);
-    });
-    const btnAll = document.createElement("li");
-    btnAll.textContent = "Tous";
-    btnAll.classList.add("btnFilter");
-    categoriesContainer.insertBefore(btnAll, categoriesContainer.firstChild);
-    
-    
-};
 import { findAllWorks } from "../services/category-service.js";
 
 export async function createWorksContainer(){
@@ -53,4 +30,39 @@ export async function createWorksContainer(){
         console.error("Error fetching or displaying works:", error);
     
     };
+};
+
+
+// fonction pour ajouter les filtres//
+
+
+import { findAllCategories } from "../services/category-service.js";
+
+export const createCategoriesContainer= async () => {
+    const categoriesContainer = document.createElement("ul");
+    categoriesContainer.classList.add("listeFilter");
+    const worksSection = document.getElementById("portfolio");
+    
+    console.log(categoriesContainer, worksSection);
+    const categories = await findAllCategories();
+    console.log(categories);
+    worksSection.appendChild(categoriesContainer);
+    //ajout du li//
+    categories.forEach(category => {
+        const btnFilter = document.createElement("li");
+        btnFilter.textContent = category.name;
+        btnFilter.classList.add("btnFilter");
+        categoriesContainer.appendChild(btnFilter);
+    });
+    btnFilter.addEventListener("click", () => {
+        const picsFilter = pics.findAllCategories(function (categories) {
+            return categories.category
+        })
+        });
+    
+    // Ajout btn "Tous" //
+    const btnAll = document.createElement("li");
+    btnAll.textContent = "Tous";
+    btnAll.classList.add("btnFilter");
+    categoriesContainer.insertBefore(btnAll, categoriesContainer.firstChild); 
 };
