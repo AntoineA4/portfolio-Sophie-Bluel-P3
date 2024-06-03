76a4409d92a9ae3ec12002da5d5146d15ddf134d
Antoine Gadrat <antoinegadrat@hotmail.fr>
2024-06-03 22:13:35 +0200

    feat: start to create the modal

--- FrontEnd/assets/scripts/components/work-component.js
+++ FrontEnd/assets/scripts/components/work-component.js
@@ -15,8 +15,6 @@ export async function createWorksContainer(works){
         imageElement.alt = title;
         const titleElement = document.createElement("h3");
         titleElement.textContent = title;
-    
-
         figureElement.appendChild(imageElement);
         figureElement.appendChild(titleElement);
         gallery.appendChild(figureElement);
@@ -31,15 +29,36 @@ export async function createWorksContainer(works){
 export const bindCreateWorkModal = () => {
     const modalButton = document.getElementById ("openModal");
     if (modalButton) {
-        modalButton.addEventListener ("click", () => {
-            const modal = document.createElement ("div")
-            modal.style.position = "fixed"
-            modal.style.top = "0" 
-            modal.style.left = "0" 
-            modal.style.background = "red"
-            modal.style.height = "100%"
-            modal.style.width = "100%"
-            document.body.appendChild (modal)
-        })
-    }
-}
+        modalButton.addEventListener ("click", async () => {
+            // create modal backdrop
+            const backdrop = document.createElement("div");
+            backdrop.classList.add("custom-modal-backdrop");
+            document.body.appendChild(backdrop);
+            // create modal box
+            const modal = document.createElement ("div");
+            modal.classList.add("modal-wrapper");
+            backdrop.appendChild(modal);
+            // create title 
+            const modalTitle = document.createElement("h2");
+            modalTitle.textContent = "Galerie photo";
+            modal.appendChild(modalTitle);
+            // works
+            const galleryWorks = document.createElement ("div");
+            galleryWorks.classList.add("gallery-modal");
+            modal.appendChild(galleryWorks);
+            const works = await findAllWorks (); 
+            works.forEach((work)=> {
+                const worksId = work.id;
+                const figure = document.createElement("figure");
+                figure.classList.add ("figure");
+                const img = document.createElement("img");
+                const trashPic = document.createElement("i");
+                trashPic.classList.add("fa-solid", "fa-trash-can");
+                img.src = work.imageUrl;
+                galleryWorks.appendChild(figure);
+                figure.appendChild(img);
+                figure.appendChild(trashPic);
+            });
+        });
+    };
+};
